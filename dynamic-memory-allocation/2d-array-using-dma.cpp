8818ab21c4e23e2b4e9856c6d1d5de93fd2283d9
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-07-30 21:11:12 +0530

    Update 2d-array-using-dma.cpp
    Added comments on the processes executed in this program

--- dynamic-memory-allocation/2d-array-using-dma.cpp
+++ dynamic-memory-allocation/2d-array-using-dma.cpp
@@ -3,27 +3,27 @@ using namespace std;
 int main()
 {
     int row, col;
-    cin >> row >> col;
+    cin >> row >> col; // Taking row and coloumn input
 
-    int **arr = new int *[row];
+    int **arr = new int *[row]; // Allocating memory for rows
 
     for (int i = 0; i < row; i++)
-        arr[i] = new int[col];
+        arr[i] = new int[col]; // Allocating memory for coloumns
 
     for (int i = 0; i < row; i++)
         for (int j = 0; j < col; j++)
-            cin >> arr[i][j];
+            cin >> arr[i][j]; // Taking input
 
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
-            cout << arr[i][j] << " ";
+            cout << arr[i][j] << " "; // printing matrix
         cout << endl;
     }
 
     for (int i = 0; i < row; i++)
-        delete arr[i];
-    delete arr;
+        delete arr[i]; // De-Allocation
+    delete arr; // De-Allocation
 
     return 0;
 }
