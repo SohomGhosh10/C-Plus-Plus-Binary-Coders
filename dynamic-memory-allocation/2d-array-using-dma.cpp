7265d8b99871d0c1cf3c548c995c1b17fe4140cf
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-07-30 21:12:27 +0530

    Update 2d-array-using-dma.cpp

--- dynamic-memory-allocation/2d-array-using-dma.cpp
+++ dynamic-memory-allocation/2d-array-using-dma.cpp
@@ -12,7 +12,7 @@ int main()
 
     for (int i = 0; i < row; i++)
         for (int j = 0; j < col; j++)
-            cin >> arr[i][j]; // Taking input
+            cin >> arr[i][j]; // Taking input of the matrix
 
     for (int i = 0; i < row; i++)
     {
